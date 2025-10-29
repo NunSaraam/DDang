fef9ed2b64ba6f2d203085d380cc65763f587634
NunSaraam <yjc22950037@gmail.com>
2025-10-29 16:21:39 +0900

    [Feat] 플레이어 시점 변경

--- DDang/Assets/Scripts/Camera/PlayerDualCamera.cs
+++ DDang/Assets/Scripts/Camera/PlayerDualCamera.cs
@@ -7,11 +7,14 @@ public class PlayerDualCamera : MonoBehaviour
     public Transform target;
     public float followSpeed = 5f;
 
-    private Vector3 offset = new Vector3(0, 10, 0);
+    private Vector3 offset = new Vector3(4f, 3f, 0f);
+    
 
     private void LateUpdate()
     {
-        Vector3 targetPosition = target.position + offset;
+        Vector3 targetPosition = target.position + offset;     
         transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+
+        transform.rotation = Quaternion.Euler(30f, -90f, 0f);
     }
 }
